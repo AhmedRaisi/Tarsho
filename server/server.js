de91b00dc519f6f31c2b7e1585d81d35007f5ae8
alrai014 <alrai014@umn.edu>
2023-11-24 14:54:07 -0600

    docker fully working

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,23 +13,17 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Serve static files from the mounted React build directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/api', (req, res) => {
-  res.json({
-    message: 'Available API Endpoints',
-    endpoints: [
-      // ... add your API endpoints here
-    ],
-  });
+// Sample API endpoint
+app.get('/api/someendpoint', (req, res) => {
+  const responseData = {
+    message: 'This is a sample API endpoint',
+    data: { key: 'value' },
+  };
+  res.json(responseData);
 });
 
-// The "catchall" handler: for any request that doesn't match one above,
-// send back React's index.html file from the mounted build directory
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// Update the proxy middleware to point to the frontend service
+app.use('/', createProxyMiddleware({ target: 'http://frontend:3000', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
