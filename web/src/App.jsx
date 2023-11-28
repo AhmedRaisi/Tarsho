f09d7421b5fe1e97fca5f805aae0e13902f9d5f1
alrai014 <alrai014@umn.edu>
2023-11-28 12:12:58 -0600

    added client profile settings

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/pages/home/HomePage';
 import ClientHomePage from './components/pages/client/ClientHomePage';
 import ProviderHomePage from './components/pages/provider/ProviderHomePage';
+import ClientSettingsPage from './components/pages/client/ClientSettingsPage';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/client" element={<ClientHomePage />} />
-        <Route path="/provider" element={<ProviderHomePage />} />
+        <Route path="/provider" element={<ProviderHomePage />} />\
+        <Route path="/clientsettings" element={<ClientSettingsPage />} />
       </Routes>
     </Router>
   );
--- web/src/components/pages/client/hf/header/header.jsx
+++ web/src/components/pages/client/hf/header/header.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import './heade.css'; // Importing CSS
-import { HashLink as Link } from 'react-router-hash-link'; // Importing HashLink
+import './heade.css'; // Ensure the CSS file name is correct
+import { HashLink as Link } from 'react-router-hash-link'; // Importing HashLink for smooth scrolling
+import { Link as RouterLink } from 'react-router-dom'; // Importing Link for standard navigation
 
 const Header = () => (
   <header className="headerr">
     <div className="header-content">
       <h1>TarshoClient</h1>
       <nav>
-        <Link smooth to="/#home">
+      <RouterLink to="/client">
           Dashboard
-        </Link>
+        </RouterLink>
         <Link smooth to="/#about">
           Services
         </Link>
-        <Link smooth to="/#services">
+        <Link smooth to="/#messages">
           Messages
         </Link>
-        <Link smooth to="/#contact">
+        <RouterLink to="/clientsettings">
           Settings
-        </Link>
-        <Link smooth to="/#services">
+        </RouterLink>
+        <Link smooth to="/#logout">
           Logout
         </Link>
       </nav>
