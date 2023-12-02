babe997363ab6e8b49569bcd5ed75c9fa6d118a4
alrai014 <alrai014@umn.edu>
2023-12-02 17:12:06 -0600

    STABLE VERSION

--- prepare.js
+++ prepare.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
+const fs = require('fs');
+const { exec } = require('child_process');
+
 if (fs.existsSync('.git')) {
   exec('npx husky install', (error, stdout, stderr) => {
     if (error) {
@@ -10,4 +13,6 @@ if (fs.existsSync('.git')) {
     console.log(`stdout: ${stdout}`);
     console.error(`stderr: ${stderr}`);
   });
+} else {
+  console.log('.git directory not found. Skipping Husky installation.');
 }
