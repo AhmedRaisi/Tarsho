81f9086f4f2bbfe487e0889ce273a5331ac6cc98
alrai014 <alrai014@umn.edu>
2023-12-02 09:42:34 -0600

    test coverage

--- web/src/app.test.js
+++ web/src/app.test.js
@@ -1,42 +1,35 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import HomePage from './components/pages/home/HomePage';
 import App from './App';
 
-const renderWithRouter = (ui, { route = '/' } = {}) => {
-  window.history.pushState({}, 'Test page', route);
-
-  return render(ui, { wrapper: BrowserRouter });
-};
 
 describe('HomePage Component Tests', () => {
   test('renders the hero section with main text', () => {
-    renderWithRouter(<App />);
+    render(<App />);
     const mainText = screen.getByText(/Empowering Your Independence, Enriching Your Choices/i);
     expect(mainText).toBeInTheDocument();
   });
 
   test('renders the mission section', () => {
-    renderWithRouter(<App />);
+    render(<App />);
     const missionSection = screen.getByText(/Our Mission/i);
     expect(missionSection).toBeInTheDocument();
   });
 
   test('renders the services section', () => {
-    renderWithRouter(<App />);
+    render(<App />);
     const servicesSection = screen.getByText(/Services/i);
     expect(servicesSection).toBeInTheDocument();
   });
 
   test('renders the contact section', () => {
-    renderWithRouter(<App />);
+    render(<App />);
     const contactSection = screen.getByText(/Contact Us/i);
     expect(contactSection).toBeInTheDocument();
   });
 
   test('opens login modal on client login button click', () => {
-    renderWithRouter(<App />);
+    render(<App />);
     const clientLoginButton = screen.getByText(/Client Login/i);
     fireEvent.click(clientLoginButton);
     // Check if the login modal is rendered with "client" role
@@ -45,7 +38,7 @@ describe('HomePage Component Tests', () => {
   });
 
   test('opens register modal on "Get Started" button click', () => {
-    renderWithRouter(<App />);
+    render(<App />);
     const registerButton = screen.getByText(/Get Started/i);
     fireEvent.click(registerButton);
     // Check if the register modal is rendered
