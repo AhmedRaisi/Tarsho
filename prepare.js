3c800ba467c3b57e8683233cefac6660802f7ed9
alrai014 <alrai014@umn.edu>
2023-12-02 16:57:16 -0600

    STABLE VERSION

---
+++ prepare.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+if (fs.existsSync('.git')) {
+  exec('npx husky install', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+}
