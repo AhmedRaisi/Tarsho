d23fe5342c21c9b1d7a166909562c51420908fa1
alrai014 <alrai014@umn.edu>
2023-12-02 09:36:32 -0600

    test coverage

--- web/src/app.test.js
+++ web/src/app.test.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import HomePage from './components/pages/home/HomePage';
 import App from './App';
 
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+
+  return render(ui, { wrapper: BrowserRouter });
+};
 
 describe('HomePage Component Tests', () => {
   test('renders the hero section with main text', () => {
-    render(<App />);
+    renderWithRouter(<App />);
     const mainText = screen.getByText(/Empowering Your Independence, Enriching Your Choices/i);
     expect(mainText).toBeInTheDocument();
   });
 
   test('renders the mission section', () => {
-    render(<App />);
+    renderWithRouter(<App />);
     const missionSection = screen.getByText(/Our Mission/i);
     expect(missionSection).toBeInTheDocument();
   });
 
   test('renders the services section', () => {
-    render(<HomePage />);
+    renderWithRouter(<HomePage />);
     const servicesSection = screen.getByText(/Services/i);
     expect(servicesSection).toBeInTheDocument();
   });
 
   test('renders the contact section', () => {
-    render(<HomePage />);
+    renderWithRouter(<HomePage />);
     const contactSection = screen.getByText(/Contact Us/i);
     expect(contactSection).toBeInTheDocument();
   });
 
   test('opens login modal on client login button click', () => {
-    render(<HomePage />);
+    renderWithRouter(<HomePage />);
     const clientLoginButton = screen.getByText(/Client Login/i);
     fireEvent.click(clientLoginButton);
     // Check if the login modal is rendered with "client" role
@@ -39,7 +45,7 @@ describe('HomePage Component Tests', () => {
   });
 
   test('opens register modal on "Get Started" button click', () => {
-    render(<HomePage />);
+    renderWithRouter(<HomePage />);
     const registerButton = screen.getByText(/Get Started/i);
     fireEvent.click(registerButton);
     // Check if the register modal is rendered
