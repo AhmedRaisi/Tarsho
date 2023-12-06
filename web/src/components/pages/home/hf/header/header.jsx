b1563c38b9f55f79288fb4e1b4d59f376f96b37e
Abdulrahman Al Hosni (CCED) <AalHosni@CCED.COM.OM>
2023-12-06 16:08:24 +0400

    home page header minor adjustment

--- web/src/components/pages/home/hf/header/header.jsx
+++ web/src/components/pages/home/hf/header/header.jsx
@@ -1,44 +1,38 @@
-import React, { useState } from 'react'
-import { HashLink as Link } from 'react-router-hash-link'
-import './hstyles.css'
-import LoginModal from './../../LoginModal.jsx'
+import React, { useState } from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+import './hstyles.css';
+import LoginModal from './../../LoginModal.jsx';
+import TarshoLogo from './../../../../../assets/TarshoLogoConcept.png'; // Make sure the path is correct
 
 const Header = () => {
-  const [showLogin, setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
 
-  // Function to open the login modal
   const openLoginModal = () => {
-    setShowLogin(true)
-  }
+    setShowLogin(true);
+  };
 
   return (
     <>
       <header className='headers'>
         <div className='header-content'>
-          <h1>Tarsho</h1>
+          {/* Logo and Text Container */}
+          <Link to='/#home' className='logo-container'>
+            <img src={TarshoLogo} alt='Tarsho Logo' className='header-logo' />
+            <span className='logo-text'>Tarsho</span>
+          </Link>
           <nav>
-            <Link smooth to='/#home'>
-              Home
-            </Link>
-            <Link smooth to='/#about'>
-              About
-            </Link>
-            <Link smooth to='/#services'>
-              Services
-            </Link>
-            <Link smooth to='/#contact'>
-              Contact
-            </Link>
-            <button onClick={openLoginModal} className='login-button'>
-              Login
-            </button>
+            <Link smooth to='/#home'>Home</Link>
+            <Link smooth to='/#about'>About</Link>
+            <Link smooth to='/#services'>Services</Link>
+            <Link smooth to='/#contact'>Contact</Link>
+            <button onClick={openLoginModal} className='login-button'>Login</button>
           </nav>
         </div>
       </header>
 
       {showLogin && <LoginModal onClose={() => setShowLogin(false)} />}
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
